8ccb4ef77b7b9bcdecb887f4ef92945191218a5f
El Mehdi <louihranim@gmail.com>
2023-06-17 18:58:06 +0100

    init v2

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,8 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
+import Head from "next/head";
 
 export default function Page() {
   return (
     <div className="grid min-h-[100vh] place-items-center">
+      <Head>
+        <title>Login Page</title>
+        <meta name="description" content="Login to PFE hamza" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <SignIn />
     </div>
   );
